e69ef91dd3a477ccd9ef63f76b2624ffb4f6dad5
/home/gabriele <gpernoca@gmail.com>
2025-03-22 00:29:21 +0000

    Initial Commit

---
+++ NovelTTS/NovelTTSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovelTTSApp.swift
+//  NovelTTS
+//
+//  Created by Gabriele Pernoca on 22/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NovelTTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NovelTTSTests/NovelTTSTests.swift
@@ -0,0 +1,16 @@
+//
+//  NovelTTSTests.swift
+//  NovelTTSTests
+//
+//  Created by Gabriele Pernoca on 22/03/2025.
+//
+
+import Testing
+
+struct NovelTTSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
